6205a001cba17e0fd0e30078bf8fc89698fce1c4
Сергей Грищенов <grishenovsv@rushydro.ru>
2025-02-08 18:11:31 +0300

    add controller /

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,8 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
 repositories {
@@ -20,6 +22,10 @@ repositories {
 dependencies {
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 testing {
