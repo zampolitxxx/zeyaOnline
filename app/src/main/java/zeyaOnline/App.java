7d612099cdd627fd1da3e13449cc968e1815061f
Сергей Грищенов <grishenovsv@rushydro.ru>
2025-02-09 12:21:07 +0300

    add model Page

--- app/src/main/java/zeyaOnline/App.java
+++ app/src/main/java/zeyaOnline/App.java
@@ -2,19 +2,31 @@ package zeyaOnline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import zeyaOnline.model.Page;
 
 @SpringBootApplication
 @RestController
 public class App {
+    private List<Page> pages = new ArrayList<>();
+
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "hello";
+    @PostMapping("/pages")
+    public Page create(@RequestBody Page page) {
+        pages.add(page);
+        return page;
+    }
+
+    @GetMapping("/pages")
+    public List<Page> get(@RequestParam(defaultValue = "1") Integer limit) {
+        return pages.stream().limit(limit).toList();
     }
 }
--- app/src/main/java/zeyaOnline/model/Page.java
+++ app/src/main/java/zeyaOnline/model/Page.java
@@ -1,4 +1,14 @@
 package zeyaOnline.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Page {
+    private String slug;
+    private String name;
+    private String body;
 }
